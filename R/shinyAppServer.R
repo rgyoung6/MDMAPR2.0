1e610ef179fb672a3f20d4ce6af027cfd5e8bad0
Abi Yogasekaram <abiyogasekaram@Abis-MacBook-Air.local>
2022-10-11 20:51:20 -0400

    changing location to outside the function.

--- R/shinyAppServer.R
+++ R/shinyAppServer.R
@@ -76,14 +76,14 @@
 #' @importFrom bslib is_bs_theme
 NULL
 
-shinyAppServer <- function(input, output, session) {
+source('table_creation_helpers.R')
+source('standard_curve_helpers.R')
+source('data_processing_helpers.R')
+source('file_validation_helpers.R')
+source('threshold_calc_helpers.R')
+source('qPCR_overview_helpers.R')
 
-  source('table_creation_helpers.R')
-  source('standard_curve_helpers.R')
-  source('data_processing_helpers.R')
-  source('file_validation_helpers.R')
-  source('threshold_calc_helpers.R')
-  source('qPCR_overview_helpers.R')
+shinyAppServer <- function(input, output, session) {
 
   #Function that is opposite on 'is in' function
   '%ni%' <- Negate('%in%')
