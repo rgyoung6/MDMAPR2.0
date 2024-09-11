2d7f6a0b4c1e70c42462400795a505bc34a5500f
rgyoung6 <rgyoung6@gmail.com>
2024-09-11 13:29:27 -0400

    Correct spelling and remove link

--- R/shinyAppUI.R
+++ R/shinyAppUI.R
@@ -26,15 +26,15 @@ shinyAppUI <- shinydashboard::dashboardPage(
       shinydashboard::tabItem(tabName = "welcomepage",
         shiny::h1(shiny::strong("Resources")),
         shiny:: br(),
-        shiny::fluidRow(shinydashboard::box(
-          title = shiny::p("Vignettes", style = "font-size:16px;"),
-          status = "primary",
-          solidHeader = TRUE,
-          collapsible = TRUE,
-          collapsed = TRUE,
-          width = 12,
-          shiny::p("Information on the vignettes will go here."))
-        ),
+        # shiny::fluidRow(shinydashboard::box(
+        #   title = shiny::p("Vignettes", style = "font-size:16px;"),
+        #   status = "primary",
+        #   solidHeader = TRUE,
+        #   collapsible = TRUE,
+        #   collapsed = TRUE,
+        #   width = 12,
+        #   shiny::p("Information on the vignettes will go here."))
+        # ),
         shiny::fluidRow(shinydashboard::box(
           title = shiny::p("Metadata Template", style = "font-size:16px;"),
           status = "primary",
@@ -42,12 +42,15 @@ shinyAppUI <- shinydashboard::dashboardPage(
           collapsible = TRUE,
           collapsed = TRUE,
           width = 12,
+          # shiny::p("To learn how to fill in the MDMAPR 2.0 Metadata Template please visit the ",
+          #          shiny::tags$a(href="https://github.com/rgyoung6/MDMAPR2.0", "MDMAPR 2.0 GitHub page."),  "The wiki page contains instructions on how to fill in the Metadata template excel file and has a complete guide with descriptions for each field in the metadata template.",  style = "font-size:16px;" ),
+          # shiny::downloadLink("downloadTemplate",
+          #                     shiny::p("Click Here to Download the Metadata Template",
+          #                              style = "font-size:16px; color:#F4412E; text-decoration: underline;")
+          # ))
           shiny::p("To learn how to fill in the MDMAPR 2.0 Metadata Template please visit the ",
-                   shiny::tags$a(href="https://github.com/rgyoung6/MDMAPR2.0", "MDMAPR 2.0 GitHub page."),  "The wiki page contains instructions on how to fill in the Metadata template excel file and has a complete guide with descriptions for each field in the metadata template.",  style = "font-size:16px;" ),
-          shiny::downloadLink("downloadTemplate",
-                              shiny::p("Click Here to Download the Metadata Template",
-                                       style = "font-size:16px; color:#F4412E; text-decoration: underline;")
-          ))
+                   shiny::tags$a(href="https://github.com/rgyoung6/MDMAPR2.0", "MDMAPR 2.0 GitHub page."),  "The wiki page contains instructions on how to fill in the Metadata template excel file and has a complete guide with descriptions for each field in the metadata template.",  style = "font-size:16px;" )
+        )
         ),
         shiny::fluidRow(shinydashboard::box(
           title = shiny::p("How are the System Calculated Threshold values determined?",
